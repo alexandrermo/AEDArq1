5f15dd57a5a7a967db8b50ddf6f30a4aab111da1
alexandrermo <alexandre.rmo94@gmail.com>
2020-06-03 08:37:49 -0300

    Feitos os erros funcionarem

--- processador2.asm
+++ processador2.asm
@@ -4,14 +4,25 @@ global executa
 global R1 ; MUDARAQUI
 global R2 ;MUDARAQUI
 
-extern printf ;MUDARAQUI
+global existeErro;MUDARAQUI
+global ptErro;MUDARAQUI
+global posMemErro
+
+
 
 NULL  equ 0 
 segment .data
-passou: dq "passou",10,0
+;Variáveis de erro
+	posMemErro: dw 0 ;posição da memória que deu erro
+	ptErro: dq 0 ;ponteiro para a string do erro
+	;Strings de erro
+	erroParam: db "Parametro(s) invalido(s) na instrucao da posição de memória ",0
+	erroPrivate: db "Tentativa de acessar registrador privado na instrucao da posição de memória ",0
+
+	existeErro: db 0 ; bool do erro
 
 mem: times 65536 db 0
-regs: times 17  db 0 
+regs: times 16  db 0 
 R1 equ $regs+0
 R2 equ  $regs+1
 R3 equ  $regs+2 
@@ -26,8 +37,7 @@ NEGATIVO   equ  $regs+14
 CARRY  equ  $regs+15 
 ;indica 8 ou 16
 x8bits:  db 0
-desvios: dq $OP_ADD
-		; 			,$SUB_OP,$AND_OP,$XOR_OP,$OR_OP,$NOT,$CMP_OP
+desvios: dq $ADD_OP;,$SUB_OP;,$AND_OP,$XOR_OP,$OR_OP,$NOT,$CMP_OP
         ;  dq $SHL_OP,$SHR_OP
         ;  dq $PUSH_OP,$POP_OP
         ;  dq  $JMP_OP,$JZ_OP,$JNZ_OP,$JL_OP,$JLE_OP,$JG_OP,$JGE_OP,$JC_OP
@@ -40,6 +50,7 @@ segment .text
     executa: 
           mov rdi, mem
 		  mov rsi, 0 ;RSI SERÁ nosso registrador IP
+		  mov r8, 0 ;O R8 será nosso bool erro
     eterno:
 		  mov rdx,0
           mov dl, byte[mem+rsi]
@@ -57,7 +68,7 @@ segment .text
 ;Considera��es	REG1 e REG2 devem ser ou de 8 ou de 16 bits os dois. Caso a soma ultrapasse a capacidade de armazenamento de REG1 o flag CARRY � setado (1).
 ;Se o bit de mais alta ordem de REG1 for setado NEGATIVO � setado (1)
 ;Se REG1==0 -> ZERO=1 
-OP_ADD:
+ADD_OP:
 ; obtem o codigo dos  registradores e verifica se sao iguais.
 ; decodifica op1
 	mov rax, 0
@@ -65,57 +76,100 @@ OP_ADD:
 	mov rcx, 0
 
 	mov al,byte[rdi+rsi+1] ; op1
-        call deco_reg
-        push cx ; 8 ou 16
+	call deco_reg
+
+	;verifica se ocorreu erro no deco_reg
+	cmp r8,1
+	je RETORNO
+
+	push cx ; 8 ou 16
 	push rax ; ponteiro area
 ;decodifica op2
 	mov al,byte [rdi+rsi+2] ; op2
-        call deco_reg
+	call deco_reg
  	; cl e rax tem valores.
 ; compara.
+
 	pop rbx
 	pop dx
+	
+	;verifica se ocorreu erro no deco_reg
+	cmp r8,1
+	je RETORNO
+
+	
 	; dois iguais ?
 	cmp cl,dl
 
-	je   MesmoTamReg
-	; tam diferentes
-	; je INVAL_INSTRUCTIONMUDARAQUI
-MesmoTamReg:
-	; RAX aponta para op2 e RBX para op1 pode ser 8 ou 16
-	cmp cl,0
-	jne xx16bitsreg
-	; 8 bits
-	; do :-)
-	mov cl,byte[RAX]
-	mov dl,byte[RBX]
-	add dl,cl
-	mov  byte [RBX],dl
-	call testaflag
-	jmp fim_ADD
-xx16bitsreg:
-	mov cx,word [RAX]
-	mov dx, word [RBX]
-	add dx,cx
-	mov  byte [RBX],dl
-	call testaflag
-fim_ADD:
-	add si,3
-	mov word[xIP],si
-	jmp eterno
+	jne INVAL_INSTRUCTION_PARAM
+	;MesmoTamReg:
+		; RAX aponta para op2 e RBX para op1 pode ser 8 ou 16
+		cmp cl,0
+		jne xx16bitsreg
+		; 8 bits
+		; do :-)
+		mov cl,byte[RAX]
+		mov dl,byte[RBX]
+		add dl,cl
+		mov  byte [RBX],dl
+		call testaflag
+		jmp fim_ADD
+	xx16bitsreg:
+		mov cx,word [RAX]
+		mov dx, word [RBX]
+		add dx,cx
+		mov  byte [RBX],dl
+		call testaflag
+	fim_ADD:
+		add si,3
+		mov word[xIP],si
+		jmp eterno
+
+
+
+RETORNO:
+	ret
+
+
+
+
+
+
+
+
+
+INVAL_INSTRUCTION_PARAM:
+	;Se algum parâmetro da instrução for inválido
+	mov r8, 1
+	mov byte[existeErro], r8b
+	mov rax, erroParam
+	mov qword[posMemErro],rsi
+	mov qword[ptErro], rax
+	ret
+
+PRIVATE_REGISTER:
+	;Se tentou acessar um registrador privado
+	mov r8, 1
+	mov byte[existeErro], r8b
+	mov rax, erroPrivate
+	mov qword[posMemErro],rsi
+	mov qword[ptErro], rax
+	ret
+
+
 
 deco_reg:
-; IN al= c�digo reg
-; OUT cl=0 8 bits  cl=1 16 bit 
-;	RAX= endereco da area do registrador. 
-; obs:  acesso a xIP e FLAGS eh invalida
+	; IN al= c�digo reg
+	; OUT cl=0 8 bits  cl=1 16 bit 
+	;	RAX= endereco da area do registrador. 
+	; obs:  acesso a xIP e FLAGS eh invalida
 	cmp al,7
-	je deco_reg
+	je PRIVATE_REGISTER;MUDARAQUI
 	cmp al,8
-	jg deco_reg
-; ponteiro �rea
+	jg PRIVATE_REGISTER;MUDARAQUI
+
 	mov cl,al
-    ; mov cl,al
+	; mov cl,al
 	
 	mov rax,regs
 	add rax,rcx
@@ -125,9 +179,9 @@ deco_reg:
 	; 8 bits
 	mov rcx, 0
 	ret
-x16breg:
-	mov rcx, 1
-	ret
+	x16breg:
+		mov rcx, 1
+		ret
 
 ;-------- 
 ; see flags 
@@ -138,13 +192,13 @@ testaflag:
 	jnc zflag
 	;--- carry
 	mov byte [CARRY],bl
-zflag:	
-	jne  negflag
-	mov byte [ZERO],bl
-	mov byte [NEGATIVO], al
-	ret
-negflag:
-	mov byte [ZERO],al
-	mov byte [NEGATIVO], bl
-    ret 
-;-------
+	zflag:	
+		jne  negflag
+		mov byte [ZERO],bl
+		mov byte [NEGATIVO], al
+		ret
+	negflag:
+		mov byte [ZERO],al
+		mov byte [NEGATIVO], bl
+		ret 
+	;-------
--- teste.c
+++ teste.c
@@ -4,6 +4,9 @@
 extern int8_t mem[];
 extern int8_t R1;
 extern int8_t R2;
+extern int8_t existeErro;
+extern char* ptErro;
+extern int16_t posMemErro;
 extern void executa();
 
 int main(){
@@ -12,10 +15,19 @@ int main(){
     R2 = 0x02;
     mem[0] = 0x00;
     mem[1] = 0x00;
-    mem[2] = 0x01;
+    mem[2] = 0x07;
     mem[3] = 0x27;
     
     executa();
 
-    printf("%d", R1);
+    if  (existeErro){ //Verifica se existe erro
+        int i = 0;
+        while(ptErro[i] != 0){
+            printf("%c", ptErro[i]); //Printa a string do erro
+            i++;
+        }
+        printf("%d\n", posMemErro); //Printa a posição de memória do erro
+    }
+    printf("%d\n", R1);
+    return 0;
 }
